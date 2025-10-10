a39dfcb89522ff700bcbe16d88a5c71cbcfc4073
Manish Bhatt <manishbhatt594@gmail.com>
2025-10-10 15:35:51 +0530

    Add code for "Be The Compiler" exercises of Chapter 2

---
+++ book.code.along/src/hfjava/code/ch02/StreamingSongTestDrive.java
@@ -0,0 +1,27 @@
+package hfjava.code.ch02;
+
+class StreamingSong {
+
+	String title;
+	String artist;
+	int duration;
+
+	void play() {
+		System.out.println("Playing song");
+	}
+
+	void printDetails() {
+		System.out.println("This is " + title + " by " + artist);
+	}
+}
+
+public class StreamingSongTestDrive {
+
+	public static void main(String[] args) {
+		StreamingSong song = new StreamingSong();
+		song.artist = "The Beatles";
+		song.title = "Come Together";
+		song.play();
+		song.printDetails();
+	}
+}
