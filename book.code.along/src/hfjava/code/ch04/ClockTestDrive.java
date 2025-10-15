08ac261a14a58bbb120d1d5c772066bca012ff0f
Manish Bhatt <manishbhatt594@gmail.com>
2025-10-15 16:36:10 +0530

    Add example & exercise code for Ch 4 - How Objects Behave

---
+++ book.code.along/src/hfjava/code/ch04/ClockTestDrive.java
@@ -0,0 +1,24 @@
+package hfjava.code.ch04;
+
+class Clock {
+	String time;
+
+	void setTime(String t) {
+		time = t;
+	}
+
+	String getTime() {
+		return time;
+	}
+}
+
+public class ClockTestDrive {
+
+	public static void main(String[] args) {
+		Clock c = new Clock();
+
+		c.setTime("1245");
+		String tod = c.getTime();
+		System.out.println("Time: " + tod);
+	}
+}
---
+++ book.code.along/src/hfjava/code/ch04/ElectricGuitar.java
@@ -0,0 +1,31 @@
+package hfjava.code.ch04;
+
+public class ElectricGuitar {
+	String brand;
+	int numOfPickups;
+	boolean rockStarUsesIt;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getNumOfPickups() {
+		return numOfPickups;
+	}
+
+	public void setNumOfPickups(int numOfPickups) {
+		this.numOfPickups = numOfPickups;
+	}
+
+	public boolean isRockStarUsesIt() {
+		return rockStarUsesIt;
+	}
+
+	public void setRockStarUsesIt(boolean rockStarUsesIt) {
+		this.rockStarUsesIt = rockStarUsesIt;
+	}
+}
